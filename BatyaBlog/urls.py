a09429de789bfac03a7d54987b32a885e26c3bce
igrekoni <igrekoni@gmail.com>
2017-09-04 20:22:40 +0300

    template basic inheritance

--- BatyaBlog/urls.py
+++ BatyaBlog/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from posts import views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include("posts.urls", namespace='posts')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
