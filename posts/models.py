1c5b0937e08054bd219fe54575f3e9c22fb27653
igrekoni <igrekoni@gmail.com>
2018-02-08 18:46:09 +0300

    refactor db fields

--- posts/models.py
+++ posts/models.py
@@ -14,14 +14,14 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    title = models.CharField(max_length=120, blank=False)
+    title = models.CharField(max_length=150, blank=False)
     image = models.ImageField(upload_to=upload_location,
                               null=True, blank=True, height_field='height_field', width_field='width_field')
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
     previewText = models.TextField(max_length=260, blank=True)
     fullText = models.TextField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
